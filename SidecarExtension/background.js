5da4796126b4cdf29b21cfe91f91f7535909843d
Dave Risney <davris@ntdev.microsoft.com>
2023-05-31 23:27:35 -0700

    WIP: Add additional actions, minimal doc of actions, refactor to support errors better

--- SidecarExtension/background.js
+++ SidecarExtension/background.js
@@ -1,4 +1,21 @@
-var port = chrome.runtime.connectNative('net.deletethis.myhost');
+/*
+
+msg in
+    sourceId - unique ID of sender
+    messageId - ID of message
+    action - action string enum
+    args - JSON object action specific args
+
+msg out
+    targetId - unique ID of sender (matches msg in)
+    messageId - ID of message (matches msg in)
+    success - boolean if it was success or not
+    result - JSON result object for success or error text for failure
+    
+
+*/
+
+const port = chrome.runtime.connectNative('net.deletethis.myhost');
 port.onMessage.addListener(function (msg) {
     console.log('Received', msg);
     handleMessage(msg);
@@ -7,67 +24,105 @@ port.onDisconnect.addListener(function () {
     console.log('Disconnected');
 });
 
-async function searchHistory(args) {
+const nameToHandlerMap = {};
+
+// https://developer.chrome.com/docs/extensions/reference/bookmarks/#method-search
+// query
+// title (exact match)
+// url (exact match)
+nameToHandlerMap.searchBookmarks = function (args, resolve) {
+    chrome.bookmarks.search(args, resolve);
+}
+
+// https://developer.chrome.com/docs/extensions/reference/history/#method-search
+// text
+// startTime
+// endTime
+// maxResults (100)
+nameToHandlerMap.searchHistory = function (args, resolve) {
+    chrome.history.search(args, resolve);
+}
+
+// https://developer.chrome.com/docs/extensions/reference/tabs/#method-query
+// title
+// url
+nameToHandlerMap.queryTabs = function (args, resolve) {
+    chrome.tabs.query(args, resolve);
+}
+
+// https://developer.chrome.com/docs/extensions/reference/tabs/#method-update
+// tabId
+// updateProperties {
+//    active
+//    url
+// }
+nameToHandlerMap.updateTab = function (args, resolve) {
+    chrome.tabs.update(args.tabId, args.updateProperties, resolve);
+}
+
+// uri
+// sizeInPixels
+nameToHandlerMap.getFavicon = function (args, resolve, reject) {
+    const faviconUri = new URL(chrome.runtime.getURL("/_favicon/"));
+    faviconUri.searchParams.set("pageUrl", args.uri);
+    faviconUri.searchParams.set("size", "" + args.sizeInPixels);
+    const faviconUriAsString = faviconUri.toString();
+
+    fetch(faviconUriAsString)
+        .then(response => response.blob())
+        .then(blob => {
+            var reader = new FileReader();
+            reader.readAsDataURL(blob);
+            reader.onload = function (e) {
+                resolve(e.target.result);
+            };
+            reader.onerror = function (e) {
+                reject(e.message);
+            }
+        }, error => {
+            reject(error);
+        });
+}
+
+function handleMessage(msg) {
     let resolve, reject;
     const promise = new Promise((resolveIn, rejectIn) => {
         resolve = resolveIn;
         reject = rejectIn;
     });
-    chrome.history.search(args, result => {
-        const resultObj = { result };
-        resolve(resultObj);
-    });
-    return promise;
-}
 
-async function queryTabs(args) {
-    let resolve, reject;
-    const promise = new Promise((resolveIn, rejectIn) => {
-        resolve = resolveIn;
-        reject = rejectIn;
-    });
-
-    chrome.tabs.query(args, result => {
-        const resultObj = { result };
-        resolve(resultObj);
-    });
-
-    return promise;
-}
-
-async function updateTab(args) {
-    let resolve, reject;
-    const promise = new Promise((resolveIn, rejectIn) => {
-        resolve = resolveIn;
-        reject = rejectIn;
-    });
-
-    chrome.tabs.update(args.tabId, args.updateProperties, result => {
-        resolve(result);
-    });
-
-    return promise;
-}
-
-async function handleMessage(msg) {
-    switch (msg.action) {
-        case "searchHistory":
-            port.postMessage(await searchHistory(msg.args));
-            break;
-
-        case "queryTabs":
-            port.postMessage(await queryTabs(msg.args));
-            break;
-
-        case "updateTab":
-            port.postMessage(await updateTab(msg.args));
-            break;
-
-        default:
-            console.log("Unhandled message action: " + msg.action);
-            port.postMessage(msg);
-            break;
+    try {
+        const handler = nameToHandlerMap[msg.action];
+        if (handler) {
+            handler(msg.args, resolve, reject);
+        } else {
+            throw new Error("Unknown message action '" + msg.action + "'' from message " + JSON.stringify(msg));
+        }
     }
+    catch (err) {
+        // unknown message action error or other errors from extension APIs
+        reject(err.message);
+    }
+
+    promise.then(result => {
+        const reply = {
+            targetId: msg.sourceId,
+            messageId: msg.messageId,
+            success: true,
+            result
+        };
+        console.log("Success", reply);
+        port.postMessage(reply);
+    }, result => {
+        const reply = {
+            targetId: msg.sourceId,
+            messageId: msg.messageId,
+            success: false,
+            result
+        };
+        console.log("Error", reply);
+        port.postMessage(reply);
+    });
 }
 
 /*
